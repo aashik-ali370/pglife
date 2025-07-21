29a28d7dde35dfdece82b239f1151281ee77cea1
aashik-ali370 <ali8077195009@gmail.com>
2025-07-21 09:18:20 +0530

    Update head_links.php

--- head_links.php
+++ head_links.php
@@ -2,3 +2,6 @@
 <link href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" rel="stylesheet" />
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet" />
 <link href="css/common1.css" rel="stylesheet" />
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
